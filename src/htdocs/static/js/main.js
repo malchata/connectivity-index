6d41d7c7a9d868225177fdc7cc76855021ebd3e2
Jeremy Wagner <jeremy.l.wagner@gmail.com>
2017-09-15 10:07:51 -0500

    Wiping the slate clean for the Webpack migration.

--- src/htdocs/static/js/main.js
+++
@@ -1,5 +0,0 @@
-import { h, render } from "preact";
-import FilterableCountryList from "./components/FilterableCountryList";
-import stats from "./components/stats";
-
-render(<FilterableCountryList stats={stats}/>, document.getElementById("main"));
